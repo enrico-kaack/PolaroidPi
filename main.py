5c67c7847a253f4952db1a20ea583733c6740609
Stephan <44555785+stdhd@users.noreply.github.com>
2019-10-09 21:35:58 +0200

    added button menu/shutter

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ from PIL import Image
 import RPi.GPIO as GPIO
 import time
 
+shutterNumber = 21
+menuNumber = 35
+
 printer = Adafruit_Thermal("/dev/ttyUSB0", 19200, timeout=5)
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(shutterNumber, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(menuNumber, GPIO.OUT, initial=GPIO.LOW)
 
 def shutterPressed(channel):
-    print("button pressed")
-    GPIO.output(25, GPIO.HIGH)
+	print("menu button pressed")
+    i = Image.open("img.png")
+	printer.printImage(i)
+
+def menuPressed(channel):
+	print("button pressed")
+    GPIO.output(menuNumber, GPIO.HIGH)
     time.sleep(1)
-    GPIO.output(25, GPIO.LOW)
+    GPIO.output(menuNumber, GPIO.LOW)
 
     printer.test()
 
 
-GPIO.add_event_detect(10,GPIO.RISING,callback=shutterPressed)
-
-
+GPIO.add_event_detect(shutterNumber,GPIO.RISING,callback=shutterPressed)
+PIO.add_event_detect(menuNumber,GPIO.RISING,callback=menuPressed)
