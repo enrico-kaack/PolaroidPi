1e1e605911db7a94df2d60f7b0d8adee2a648fdd
enrico-kaack <e.kaack@live.de>
2019-10-09 19:37:40 +0200

    Added: test code for input, led output and printer

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from  Adafruit_Thermal import *
 from PIL import Image
 
+import RPi.GPIO as GPIO
+import time
+
 printer = Adafruit_Thermal("/dev/ttyUSB0", 19200, timeout=5)
 
-printer.test()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+
+def shutterPressed(channel):
+    print("button pressed")
+    GPIO.output(25, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(25, GPIO.LOW)
+
+    printer.test()
+
+
+GPIO.add_event_detect(10,GPIO.RISING,callback=shutterPressed)
+
+
